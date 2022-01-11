3b25470aeab29d74a1456acb0e846e1a00973264
cub525 <49500036+cub525@users.noreply.github.com>
2022-01-11 13:30:40 -0700

    Partially working state

--- tests.py
+++ tests.py
@@ -4,48 +4,93 @@ import sys
 import io
 import unittest
 
-
 class CapturePrint():
     def __enter__(self):
         captured_output = io.StringIO()
         sys.stdout = captured_output
         return captured_output
 
-    def __exit__(self, *args):
+    def __exit__(self, *_):
         sys.stdout = sys.__stdout__
 
 
-class TestTicTacToe(unittest.TestCase):
+class TestDisplay(unittest.TestCase):
     def test_display(self):
         with CapturePrint() as captured_output:
             board = tictactoe.Board()
             board.display()
             message = captured_output.getvalue()
-        self.assertEqual("1|2|3\n-+-+-\n4|5|6\n-+-+-\n7|8|9\n", message)
+        self.assertEqual("\n1|2|3\n-+-+-\n4|5|6\n-+-+-\n7|8|9\n", message)
 
+    def test_display_changes(self):
+        board = tictactoe.Board()
+        board.board[0] = 'x'
+        with CapturePrint() as captured_output:
+            board.display()
+            message = captured_output.getvalue()
+        self.assertEqual("\nx|2|3\n-+-+-\n4|5|6\n-+-+-\n7|8|9\n", message)
+
+
+class TestInteraction(unittest.TestCase):
     @patch('tictactoe.input', create=True)
-    def test_input(self, mocked_input):
-        mocked_input.side_effect = [1]
+    def test_prompt(self, mocked_input):
+        mocked_input.side_effect = ["1"]
         board = tictactoe.Board()
         board.prompt()
         with CapturePrint() as captured_output:
             board.display()
             message = captured_output.getvalue()
-        self.assertEqual("x|2|3\n-+-+-\n4|5|6\n-+-+-\n7|8|9\n", message)
+        self.assertEqual("\nx|2|3\n-+-+-\n4|5|6\n-+-+-\n7|8|9\n", message)
 
     @patch('tictactoe.input', create=True)
     def test_interact(self, mocked_input):
-            mocked_input.side_effect = [2, 5]
+            mocked_input.side_effect = ["2", "5", "q"]
             board = tictactoe.Board()
-            board.interact()
+            with CapturePrint() as _:
+                board.interact()
             expected_board = [" ", "x", " ", " ", "o", " ", " ", " ", " "] 
             self.assertEqual(expected_board, board.board)
 
-    @patch('tictactoe.input', create=True)
-    def test_win(self, mocked_input):
-        mocked_input.side_effect = [1, 4, 2, 5, 3]
-        board = tictactoe.Board()
-        for _ in range(4):
-            board.prompt()
-        with CapturePrint() as captured_output:
-            board.interact()
+    def test_interact_win(self):
+            board = tictactoe.Board()
+            board.board = ["x", "x", "x", " ", " ", " ", " ", " ", " "]
+            with CapturePrint() as captured_output:
+                board.interact()
+                message = captured_output.getvalue()
+            self.assertEqual("Good game. Thanks for playing!", message)
+
+
+
+class TestWinCondition(unittest.TestCase):
+    board = tictactoe.Board()
+    def test_win_horizontal(self):
+        self.board.board = ["x", "x", "x", " ", " ", " ", " ", " ", " "]
+        self.assertTrue(self.board.is_win())
+
+    def test_win_horizontal2(self):
+        self.board.board = [" ", " ", " ", "x", "x", "x", " ", " ", " "]
+        self.assertTrue(self.board.is_win())
+
+    def test_win_horizontal3(self):
+        self.board.board = [ " ", " ", " ", " ", " ", ", ""x", "x", "x"]
+        self.assertTrue(self.board.is_win())
+    
+    def test_win_vertical(self):
+        self.board.board = ["x", " ", " ", "x", " ", " ", "x", " ", " "]
+        self.assertTrue(self.board.is_win())
+
+    def test_win_vertical2(self):
+        self.board.board = [" ", "x", " ", " ", "x", " ", " ", "x", " "]
+        self.assertTrue(self.board.is_win())
+
+    def test_win_vertical3(self):
+        self.board.board = [ " ", " ", "x", " ", " ", "x", " ", " ", "x"]
+        self.assertTrue(self.board.is_win())
+
+    def test_win_diagonal(self):
+        self.board.board = ["x", " ", " ", " ", "x", " ", " ", " ", "x"]
+        self.assertTrue(self.board.is_win())
+
+    def test_win_diagonal2(self):
+        self.board.board = [" ", " ", "x", " ", "x", " ", "x", " ", " "]
+        self.assertTrue(self.board.is_win())
--- tictactoe.py
+++ tictactoe.py
@@ -1,26 +1,50 @@
+"""
+This script starts an interactive tic tac toe game for two players. Valid inputs are 1-9, 'q', 'quit' and 'exit'
+
+@author: Emmett Hart
+@assignment: W02 Prove: Tic Tac Toe
+"""
 class Board():
     def __init__(self) -> None:
         self.board = 9*[" "]
         self.turn = 'x'
 
     def display(self):
+        print()
         for i in range(9):
             end="\n" if i == 8 else "\n-+-+-\n" if ( i + 1 ) % 3 == 0 else "|"
             print(i + 1 if self.board[i] == " " else self.board[i], end=end) 
 
     def prompt(self):
-        # print("x's turn to choose a square (1-9): ", end="")
-        square = int( input(f"{self.turn}'s turn to choose a square (1-9): ") ) - 1
-        self.board[square] = self.turn
-        self.turn = 'o' if self.turn == 'x' else 'x'
+        choice = input(f"\n{self.turn}'s turn to choose a square (1-9): ") 
+        if choice.isalpha() and choice in ["q", "exit", "quit"]:
+            print("Quitting... ")
+            return True
+        elif choice.isdecimal():
+            square = int(choice) - 1
+            if 0 > square > 10:
+                raise ValueError(f"\"{choice}\" is not a valid input")
+            elif self.board[square] != " ":
+                raise ValueError(f"Square \"{square}\" is already filled")
+            else:
+                self.board[square] = self.turn
+                self.turn = 'o' if self.turn == 'x' else 'x'
+        else:
+            raise ValueError(f"\"{choice}\" is not a valid input\nValid Inputs are numbers 1-9, 'quit', 'q', and 'exit'")
 
     def interact(self):
-        i = 1
-        while i < 2:
+        quit_ = False
+        self.display()
+        while not (quit_ or self.is_win()):
+            try:
+                quit_ = self.prompt()
+            except ValueError as verror:
+                print(verror)
+                continue
             self.display()
-            self.prompt()
-            i += 1
 
+    def is_win(self) -> bool:
+        return self.board[0] == self.board[1] == self.board[2]
 
 def main():
     board = Board()
